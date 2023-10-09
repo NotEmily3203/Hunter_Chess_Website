39558e3422142be1be043e635a7a8084c4e483e2
NotEmily3203 <linemily62@gmail.com>
2023-10-08 23:28:40 -0400

    created components and containers organized into files

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {RiMenu3Line, RiCloseLin} from './react-icons/ri';
+import './navbar.css';
+
+//naming convention is called BEM
+
+const Navbar = () => {
+  return (
+    <div className='gpt3__navbar'>
+      <div className='gpt3__navbar-links'>
+        <div className='gpt3__navbar-links_logo'>
+          //add image
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
