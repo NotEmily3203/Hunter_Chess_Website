b8fbcbcf87b0d4a0cfdc3275f224e00310a562bc
NotEmily3203 <linemily62@gmail.com>
2023-10-10 00:45:34 -0400

    made nav bar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,15 +1,50 @@
-import React from 'react';
-import {RiMenu3Line, RiCloseLin} from './react-icons/ri';
+import React, {useState} from 'react';
+import {RiMenu3Line, RiCloseLine} from 'react-icons/ri';
+import logo from '../../assets/new_hunter_logo-removebg-preview-cropped.png'
 import './navbar.css';
 
 //naming convention is called BEM
 
+//create variable menu for navbar linked items
+// <> is a react fragment which allows html elements to be stored in a varibale and reused
+const navItems = () => {
+  return(
+  <>
+  <p><a href='#home'>Home</a></p>
+  <p><a href='#about'>About</a></p>
+  <p><a href='#events'>Events</a></p>
+  <p><a href='#officers'>Officers</a></p>
+  <p><a href='#contacts'>Contacts</a></p>
+  </>)
+}
+
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false); //Mobile view of website
   return (
-    <div className='gpt3__navbar'>
-      <div className='gpt3__navbar-links'>
-        <div className='gpt3__navbar-links_logo'>
-          //add image
+    <div className='chess__navbar'>
+      <div className='chess__navbar-links'>
+        <div className='chess__navbar-links_logo'>
+          <img src={logo} alt="logo" href='#home'></img>
+          <div className='chess__navbar-links_container'>
+            {navItems()}
+          </div>
+        </div>
+        <div className='chess__navbar-sign'>
+          <p>Sign in</p>
+          <button type="button">Sign Up</button>
+        </div>
+        <div className='chess__navbar-menu'>
+          {toggleMenu
+            ? <RiCloseLine color='#fff' size={27} onClick={() => setToggleMenu(false)} />
+            : <RiMenu3Line color='#fff' size={27} onClick={() => setToggleMenu(true)} />
+          }
+          {toggleMenu && (
+            <div className='chess__navbar-menu_container scale-up-center'>
+              <div className='chess__navbar-menu-container-links'>
+                {navItems()}
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
