938f80579112c6a610849c3ac94cc7c10d0e9553
NotEmily3203 <linemily62@gmail.com>
2023-12-25 18:17:11 -0500

    navbar dynamic screen display

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,16 +7,16 @@ import './navbar.css';
 
 //create variable menu for navbar linked items
 // <> is a react fragment which allows html elements to be stored in a varibale and reused
-const navItems = () => {
-  return(
+
+const Menu =() => (
   <>
   <p><a href='#home'>Home</a></p>
   <p><a href='#about'>About</a></p>
   <p><a href='#events'>Events</a></p>
   <p><a href='#officers'>Officers</a></p>
   <p><a href='#contacts'>Contacts</a></p>
-  </>)
-}
+  </>
+)
 
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false); //Mobile view of website
@@ -25,29 +25,32 @@ const Navbar = () => {
       <div className='chess__navbar-links'>
         <div className='chess__navbar-links_logo'>
           <img src={logo} alt="logo" href='#home'></img>
-          <div className='chess__navbar-links_container'>
-            {navItems()}
-          </div>
         </div>
-        <div className='chess__navbar-sign'>
-          <p>Sign in</p>
-          <button type="button">Sign Up</button>
-        </div>
-        <div className='chess__navbar-menu'>
-          {toggleMenu
-            ? <RiCloseLine color='#fff' size={27} onClick={() => setToggleMenu(false)} />
-            : <RiMenu3Line color='#fff' size={27} onClick={() => setToggleMenu(true)} />
-          }
-          {toggleMenu && (
-            <div className='chess__navbar-menu_container scale-up-center'>
-              <div className='chess__navbar-menu-container-links'>
-                {navItems()}
-              </div>
-            </div>
-          )}
+        <div className='chess__navbar-links_container'>
+            <Menu/>
         </div>
       </div>
-    </div>
+      <div className='chess__navbar-sign'>
+          <p>Sign in</p>
+          <button type="button">Sign Up</button>
+      </div>
+      <div className='chess__navbar-menu'>
+        {toggleMenu
+        ? <RiCloseLine color ="#fff" size = {27} onClick={()=>setToggleMenu(false)}/>
+        : <RiMenu3Line color ="#fff" size = {27} onClick={()=>setToggleMenu(true)}/>}
+        {toggleMenu && (
+          <div className='chess__navbar-menu_container scale-up-center'>
+            <div className='chess__navbar-menu_container-links'>
+              <Menu/>
+              <div className='chess__navbar-menu_container-links-sign'>
+                  <p>Sign in</p>
+                  <button type="button">Sign Up</button>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+      </div>
   )
 }
 
