46266b667063147bbd82d268c32bf3afdbd50a89
NotEmily3203 <linemily62@gmail.com>
2023-12-29 15:34:52 -0500

    heading

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import './header.css';
+import playing from '../../assets/playing-chess.jpg';
 
 const Header = () => {
   return (
-    <div>
-      Header
+    <div className='chess__header section__padding' id='home'>
+      <div className='chess__header-content'>
+        <h1 className='gradient__text'>
+          Welcome to Hunter Chess Club
+        </h1>
+        <p>Join the community of avid chess players and improve your skills! Check out our Discord and Instagram for upcoming events.</p>
+        
+      </div>
+      <div className='chess__header-image'>
+        <img src={playing} alt="playing" />
+      </div>
     </div>
   )
 }
